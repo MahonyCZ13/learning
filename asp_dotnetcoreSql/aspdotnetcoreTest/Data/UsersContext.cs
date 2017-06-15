d1c309bf4b6eda2377a9a6654c933e40147635ab
MahonyCZ13 <maronek.petr@gmail.com>
2017-06-15 18:36:41 +0200

    added models and data folders and classes

---
+++ asp_dotnetcoreSql/aspdotnetcoreTest/Data/UsersContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aspdotnetcoreTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspdotnetcoreTest.Data
+{
+    public class UsersContext
+    {
+
+    }
+}
---
+++ asp_dotnetcoreSql/aspdotnetcoreTest/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspdotnetcoreTest.Models
+{
+    public class Users
+    {
+        public string userName { get; set; }
+        public int ID { get; set; }
+    }
+}
