6f7c3db54eac9cc8fd062e5eeeafeebfb3ffa012
Petr Maronek <maronek.petr@gmail.com>
2017-09-13 10:55:30 +0200

    number reduced from 100 to 20

--- cs_tutoPoint/lessons/0012_3_nestedLoop.cs
+++ cs_tutoPoint/lessons/0012_3_nestedLoop.cs
@@ -9,9 +9,9 @@ namespace csharptuto.lessons
             Console.WriteLine("Lesson 12.3\n");
             int i, j;
 
-            for (i = 2; i < 100; i++)
+            for (i = 2; i < 20; i++)
             {
-                for (j = 2; j < 100; j++)
+                for (j = 2; j < 20; j++)
                 if ((i % j) == 0) break;
                 if (j > (i / j))
                 Console.WriteLine($"{i} is a prime number!");
