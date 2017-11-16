cf4b58db259b4e0d0eadd9fecaa3979d6981363f
Petr Maronek <MahonyCZ13@users.noreply.github.com>
2017-11-16 15:28:24 +0100

    Update sqlExamples.sql

--- sqlExamples.sql
+++ sqlExamples.sql
@@ -13,3 +13,20 @@ MariaDB [test]> source tolkien.sql
 +--------+----------------------------------------------+----------+----------+----+---------------+
 4 rows in set (0.00 sec)
 */
+
+/* Create Table */
+CREATE TABLE IF NOT EXISTS test3 (
+    id INT NOT NULL PRIMARY KEY auto_increment,
+    itemName VARCHAR(200) NOT NULL,
+    itemDesc VARCHAR(300),
+    itemDate DATE NOT NULL,
+    itemNumber INT NOT NULL
+);
+
+/* Insert Into */
+INSERT INTO 
+    test3 (itemName, itemDesc, itemDate, itemNumber)
+    VALUES
+        ('Apple','A fruit','2017-11-16',3),
+        ('Lemon','A another fruit','2017-10-18',6),
+        ('A car','Expensive object', '2017-03-01',1);
