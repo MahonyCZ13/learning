7a6310e75bb608be62e3b19076ccbf74bd3c5871
Petr Maronek <maronek.petr@outlook.com>
2019-07-22 09:58:17 +0200

    finished lesson 1

---
+++ c_tuto/1_variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+    float = 4.5;
+    double c = 5.25;
+    float sum;
+
+    sum = a + b + c;
+
+    printf("the sum of 1, b and c is %f", sum);
+    return 0;
+}
