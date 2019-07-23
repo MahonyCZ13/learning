1097ffed3ad99f2792ce0bd63d6cf89f44d049ae
Petr Maronek <maronek.petr@outlook.com>
2019-07-23 11:36:54 +0200

    started learning C++

---
+++ cpp_tuto/0_hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Print example 1: When we are not declaring the using above^
+    std::cout << "Hello world!";
+
+    // Print example 2
+    cout << "Hello world!";
+
+    return 0;
+
+}
